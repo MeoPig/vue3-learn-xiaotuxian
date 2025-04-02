d7dc54792ff514f1cd8efc12572d6988f1ba25f9
MeoPig <hzh812918537@gmail.com>
2025-04-02 15:29:03 +0800

    Pinia优化重复请求

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,15 +3,26 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue'
+
+import { useCategoryStore } from '@/stores/category'
+import { onMounted } from 'vue'
+
+const categoryStore = useCategoryStore()
+
+onMounted(() => {
+  categoryStore.getCategory()
+})
+
 </script>
 
-<template> 
+
+<template>
   <div style="height: 1200px;">
-    <LayoutFixed />
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
-  <LayoutFooter />
+  <LayoutFooter />    
   </div>
-  
+
 </template>
