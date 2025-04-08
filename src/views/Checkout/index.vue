2fca2f36186f2479dbe900a64d6f2b31f74aad2e
MeoPig <hzh812918537@gmail.com>
2025-04-08 11:30:40 +0800

    结算页-生成订单功能实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { getCheckoutInfoAPI } from '@/apis/checkout'
+import { getCheckoutInfoAPI,createOrderAPI } from '@/apis/checkout'
 import { ref,onMounted } from 'vue'
+import { useRouter} from 'vue-router'
+import { useCartStore } from '@/stores/cartStore'
+
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({})  // 地址对象
-
+const router = useRouter()
+const cartStore = useCartStore()
 const getCheckInfo = async() => {
     const res = await getCheckoutInfoAPI()
     checkInfo.value = res.result
@@ -28,6 +32,32 @@ const confirm = () => {
     curAddress.value = activeAddress.value
     toggleFlag.value = false
     activeAddress.value = {}
+}
+
+//生成订单回调
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: '',
+    goods: checkInfo.value.goods.map(item => {
+      return {
+        skuId: item.skuId,
+        count: item.count
+      }
+    }),
+    addressId: curAddress.value.id
+  })
+  const orderId = res.result.id
+  router.push({
+    path: '/pay',
+    query: {
+      id: orderId
+    }
+  })
+  // 更新购物车
+  cartStore.updateNewList()
 }
 </script>
 
@@ -123,7 +153,7 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large" >提交订单</el-button>
+          <el-button @click="createOrder()" type="primary" size="large" >提交订单</el-button>
         </div>
       </div>
     </div>
